8db731aaa21c2406a4fe90d8969e6b0a244652b4
Dominik Jurczak <dominikjjurczak@gmail.com>
2018-05-05 21:41:50 +0200

    .

--- app/src/main/java/pl/lo3/bendhelp/MainActivity.java
+++ app/src/main/java/pl/lo3/bendhelp/MainActivity.java
@@ -1,7 +1,9 @@
 package pl.lo3.bendhelp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openCreateBendLabel(View view){
+
+        Intent openCBL = new Intent(this, CreateBendLabelActivity.class);
+
+        startActivity(openCBL);
+
+    }
+
+
+
 }
